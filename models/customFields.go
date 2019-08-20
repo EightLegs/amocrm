1eddf3742677a87af2daceef3f7c47b9f9964e67
Vasiliy I Ozerov <vasiliyozerov@gmail.com>
2019-08-20 17:28:05 +0300

    Fixed customfields exports to json

--- models/customFields.go
+++ models/customFields.go
@@ -2,12 +2,14 @@ package models
 
 type (
 	CustomField struct {
-		Id     int
-		Name   string
-		Values []struct {
-			Value string
-			Enum  string
-		}
+		Id     int `json:"id"`
+		Name   string `json:"name"`
+        Values []CustomValue `json:"values"`
 		IsSystem bool `json:"is_system"`
+	}
+
+	CustomValue struct {
+	  Value string `json:"value"`
+	  Enum string `json:"enum"`
 	}
 )
